a594730e80c7317f5268f349886fe9e173707f17
Alex <highama@oregonstate.edu>
2025-06-11 12:17:17 -0700

    Add null and time providers to main.tf.  fails after not finding ansible playbook. Ansible playbook not created so this is expected.

--- main.tf
+++ main.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.2"
+    }
+    time = {
+      source = "hashicorp/time"
+      version = "~> 0.9"
+    }
   }
   required_version = ">= 1.2.0"
 }
@@ -40,4 +48,27 @@ data "aws_eip" "minecraft" {
 resource "aws_eip_association" "minecraft" {
   instance_id   = aws_instance.app_server.id
   allocation_id = data.aws_eip.minecraft.id
+}
+
+resource "time_sleep" "wait_for_instance" {
+  depends_on = [aws_eip_association.minecraft]
+  create_duration = "60s"
+}
+
+resource "null_resource" "minecraft_setup" {
+  triggers = {
+    instance_id = aws_instance.app_server.id
+    eip_id      = data.aws_eip.minecraft.id
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i '${data.aws_eip.minecraft.public_ip},' --private-key='${var.key_name}.pem' --user=ubuntu --ssh-extra-args='-o StrictHostKeyChecking=no -o ConnectTimeout=30' minecraft-setup.yml"
+    
+    environment = {
+      ANSIBLE_HOST_KEY_CHECKING = "False"
+      ANSIBLE_TIMEOUT = "30"
+    }
+  }
+
+  depends_on = [time_sleep.wait_for_instance]
 }
